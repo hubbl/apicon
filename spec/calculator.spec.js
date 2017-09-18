97c774fd03d60586a52585f5b562e323a3c73875
Sebastian Springer <info@sebastian-springer.com>
2017-09-18 10:11:34 +0200

    first test

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -1,5 +1,25 @@
+const Calculator = require('../src/calculator');
+
 describe('Calculator', () => {
     it('should work', () => {
-        expect(true).toBe(true);
+        expect(true).not.toBe(false);
+    });
+
+    it('should add 1 and 1 and return 2', () => {
+        // arrange
+        const calc = new Calculator();
+        // act
+        const result = calc.add(1, 1);
+        // assert
+        expect(result).toEqual(2);
+    });
+
+    it('should add 2 and 1 and return 3', () => {
+        // arrange
+        const calc = new Calculator();
+        // act
+        const result = calc.add(2, 1);
+        // assert
+        expect(result).toEqual(3);
     });
 });
